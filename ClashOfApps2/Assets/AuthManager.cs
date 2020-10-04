704767089c3092adc321c4a86601df7b231d4fc6
Santiago Figueroa <37884952+SantiagoFigueroaMc@users.noreply.github.com>
2020-10-04 18:40:13 -0300

    Firebase

--- ClashOfApps2/Assets/AuthManager.cs
+++ ClashOfApps2/Assets/AuthManager.cs
@@ -1,18 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Firebase;
+using Firebase.Auth;
+using TMPro;
 
 public class AuthManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //Firebase variables
+    [Header("Firebase")]
+    public DependencyStatus dependencyStatus;
+    public FirebaseAuth auth;
+    public FirebaseUser User;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    //Login variables
+    [Header("Login")]
+    public TMP_InputField emailLoginField;
+    public TMP_InputField passwordLoginField;
+    public TMP_Text warningLoginText;
+    public TMP_Text confirmLoginText;
+
+    //Register variables
+    [Header("Register")]
+    public TMP_InputField usernameRegisterField;
+    public TMP_InputField emailRegisterField;
+    public TMP_InputField passwordRegisterField;
+    public TMP_InputField passwordRegisterVerifyField;
+    public TMP_Text warningRegisterText;
 }
